407ad5d35f8190e13cff6aeeda85c92aabbf275f
Vaishant Makan <vaishantmakan@gmail.com>
2021-04-13 12:22:38 +0530

    Optimal Solution added i.e O(n) Solution

--- two-sum/two-sum.cpp
+++ two-sum/two-sum.cpp
@@ -1,4 +1,4 @@
-// O(n^2) Solution
+// O(n^2) Complexity Solution
 
 class Solution {
 public:
@@ -18,6 +18,37 @@ public:
                     
                     break;
                 }
+            }
+        }
+        return ans;
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// O(n) Complexity Solution
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        int n = nums.size();
+        vector<int>ans;
+        
+        unordered_map<int,int> m;
+        
+        for(int i=0;i<n;i++)
+        {
+            long long rem = target-nums[i];
+            
+            if(m.find(rem) != m.end())
+            {
+                ans.push_back(m[rem]);
+                ans.push_back(i);
+            }
+            else
+            {
+                m[nums[i]]=i;
             }
         }
         return ans;
