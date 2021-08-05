57fd24078d9bcbe5f77a4067a9b0cc736f1f7feb
Vaishant Makan <vaishantmakan@gmail.com>
2021-08-05 10:25:53 +0530

    Time: 212 ms (92.02%), Space: 144.5 MB (89.95%) - LeetHub

--- minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
+++ minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
@@ -11,23 +11,62 @@
  */
 class Solution {
 public:
+    
+    //optimised solution
+    
     int minDepth(TreeNode* root) {
-        
-        if(root==nullptr)
-        {
+    
+        if(root == nullptr)
             return 0;
+    
+        queue<TreeNode*> q;
+        q.push(root);
+        int depth=1;
+    
+        while(!q.empty())
+        {
+            int level_nodes = q.size();
+            for(int i=0;i<level_nodes;i++)
+            {
+                TreeNode* temp = q.front();
+                q.pop();
+            
+                if(temp->left == nullptr && temp->right == nullptr)
+                    return depth;
+            
+                if(temp->left != nullptr)
+                     q.push(temp->left);
+            
+                if(temp->right != nullptr)
+                    q.push(temp->right);
+            }
+         depth++;
         }
-        
-        int l = minDepth(root->left);
-        int r = minDepth(root->right);
-        
-        if(l==0 && r!=0)
-            return r+1;
-        else if(l!=0 && r==0)
-            return l+1;
-        else if(l==0 && r==0)
-            return 1;
-        else
-            return min(l,r)+1;
+        return depth;
     }
 };
+    
+    
+    
+// This below solution is O(n) becasue even if the leaf node i.e min depth is just next of root node even then it will check all the tree nodes .....  To make it better use bfs instead of dfs.....
+    
+// int minDepth(TreeNode* root) {
+    
+//         if(root==nullptr)
+//         {
+//             return 0;
+//         }
+        
+//         int l = minDepth(root->left);
+//         int r = minDepth(root->right);
+        
+//         if(l==0 && r!=0)
+//             return r+1;
+//         else if(l!=0 && r==0)
+//             return l+1;
+//         else if(l==0 && r==0)
+//             return 1;
+//         else
+//             return min(l,r)+1;
+//     }
+// };
