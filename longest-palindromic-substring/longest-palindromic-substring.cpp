2f103f24d981d7d50e9bad8922453881909358be
Vaishant Makan <vaishantmakan@gmail.com>
2021-08-06 14:56:22 +0530

    Time: 80 ms (62.70%), Space: 231.5 MB (20.71%) - LeetHub

---
+++ longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    
+    int expandAroundCenter(string s, int left, int right)
+    {
+        int l = left, r = right;
+        while(l>=0 && r<s.length() && s.at(l)==s.at(r))
+        {
+            l--;
+            r++;
+        }
+        
+        return r-l-1;
+    }
+    
+    string longestPalindrome(string s) {
+    
+        if(s.length() == 1)
+            return s;
+        
+        int start=0,end=0;
+        
+        for(int i=0;i<s.length();i++)
+        {
+            int len1 = expandAroundCenter(s,i,i);
+            int len2 = expandAroundCenter(s,i,i+1);
+            int len  = max(len1,len2);
+            if(len > end-start)
+            {
+                start = i-((len-1)/2);
+                end = i+(len/2);
+            }
+        }
+        return s.substr(start,end-start+1);
+        
+    }
+};
