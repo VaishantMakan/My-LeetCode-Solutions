924113862ed8730fec3c4ead9b131e02c67de65a
Vaishant Makan <vaishantmakan@gmail.com>
2021-08-06 17:49:48 +0530

    Time: 152 ms (51.46%), Space: 25.5 MB (43.62%) - LeetHub

--- longest-palindromic-substring/longest-palindromic-substring.cpp
+++ longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,37 +1,85 @@
 class Solution {
 public:
     
-    int expandAroundCenter(string s, int left, int right)
-    {
-        int l = left, r = right;
-        while(l>=0 && r<s.length() && s.at(l)==s.at(r))
-        {
-            l--;
-            r++;
-        }
-        
-        return r-l-1;
-    }
-    
+// DP Method ....Time : O(n^2) , space : O(1)    
     string longestPalindrome(string s) {
-    
-        if(s.length() == 1)
-            return s;
         
-        int start=0,end=0;
+        int n=s.size();
         
-        for(int i=0;i<s.length();i++)
+        // dp[i][j] will be 'true' if the string from index i to j is a palindrome.
+        bool dp[n][n];
+        
+        if(n==0)
+            return "";
+        
+        //Initialize with false
+        memset(dp,0,sizeof(dp));
+        
+        string ans="";
+        ans += s[0];
+        
+        for(int i=0;i<n;i++)
+            dp[i][i]=true;
+        
+        for(int i=n-1;i>=0;i--)
         {
-            int len1 = expandAroundCenter(s,i,i);
-            int len2 = expandAroundCenter(s,i,i+1);
-            int len  = max(len1,len2);
-            if(len > end-start)
+            for(int j=i+1;j<n;j++)
             {
-                start = i-((len-1)/2);
-                end = i+(len/2);
+                if(s[i]==s[j])
+                {
+                    //If it is of two character OR if its susbtring is palindrome.
+                    if(j-i == 1 || dp[i+1][j-1]==true)
+                    {
+                        //Then it will also a palindrome substring
+                        dp[i][j]=true;
+                        
+                        if(ans.size() < j-i+1)
+                        {
+                            //Check for Longest Palindrome substring
+                            ans = s.substr(i,j-i+1);
+                        }
+                    }
+                }
             }
         }
-        return s.substr(start,end-start+1);
-        
+        return ans;
     }
 };
+    
+    
+//  Expand Around Center method .... Time Complexity : O(n^2), space : O(1)
+    
+//     int expandAroundCenter(string s, int left, int right)
+//     {
+//         int l = left, r = right;
+//         while(l>=0 && r<s.length() && s.at(l)==s.at(r))
+//         {
+//             l--;
+//             r++;
+//         }
+        
+//         return r-l-1;
+//     }
+    
+//     string longestPalindrome(string s) {
+    
+//         if(s.length() == 1)
+//             return s;
+        
+//         int start=0,end=0;
+        
+//         for(int i=0;i<s.length();i++)
+//         {
+//             int len1 = expandAroundCenter(s,i,i);
+//             int len2 = expandAroundCenter(s,i,i+1);
+//             int len  = max(len1,len2);
+//             if(len > end-start)
+//             {
+//                 start = i-((len-1)/2);
+//                 end = i+(len/2);
+//             }
+//         }
+//         return s.substr(start,end-start+1);
+        
+//     }
+//};
