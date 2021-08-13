27dc58ee75b86f03193f59520f01be75cae73deb
Vaishant Makan <vaishantmakan@gmail.com>
2021-08-13 17:37:04 +0530

    Explanation added

--- counting-bits/counting-bits.cpp
+++ counting-bits/counting-bits.cpp
@@ -13,3 +13,5 @@ public:
         return mem;
     }
 };
+
+//Explanation : If number i is even then it will have the same number of 1's bits as the number i/2 . If its odd then it will have 1 + num of 1's bits in i/2 
