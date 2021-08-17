0deaf96a7cae44c6fe71d0dba58676a9dd29970d
Vaishant Makan <vaishantmakan@gmail.com>
2021-08-17 19:16:24 +0530

    Time: 56 ms (93.60%), Space: 19.9 MB (96.41%) - LeetHub

--- 3sum/3sum.cpp
+++ 3sum/3sum.cpp
@@ -2,32 +2,87 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         
+//    Using two pointer Method
         int n=nums.size();
         vector<vector<int>>ans;
-        unordered_map<int,int> m;
         
         sort(nums.begin(),nums.end());
         
-        for (int i = 0; i < n; i++) 
-            m[nums[i]] = i;
+        if(nums.empty())
+            return ans;
         
         for(int i=0;i<n;i++)
         {
+            
+            if(nums[i]>0)
+                break;      // all numbers from now on will be greater than 0, no point in continuing
+            
+            // we have seen this number & combo before; skip
             if(i>0 && nums[i]==nums[i-1])
                 continue;
             
-            for(int j=i+1;j<n;j++)
+            int l=i+1,r=n-1;
+            while(l<r)
             {
-                if(j!=i+1 && nums[j]==nums[j-1])
-                    continue;
-                
-                if(m[-(nums[i]+nums[j])] > j)
+                int sum=nums[i]+nums[l]+nums[r];
+                if(sum==0)
                 {
-                    ans.push_back({nums[i],nums[j],-(nums[i]+nums[j])});
-                    continue;
+                    ans.push_back({nums[i],nums[l],nums[r]});
+                    int last_left=nums[l], last_right=nums[r];
+                    
+                    // we have seen this number & combo before; skip
+                    while(l<r && nums[l]==last_left)
+                        l++;
+                    
+                    while(l<r && nums[r]==last_right)
+                        r--;
                 }
+                else if(sum < 0)
+                {
+                    l++;
+                }
+                else
+                {
+                    r--;
+                }
+                
             }
         }
         return ans;
     }
-};
+};   
+        
+        
+//    Using HashMaps .... same time complexity (O(n^2)) but is slower...
+        
+//         int n=nums.size();
+//         vector<vector<int>>ans;
+//         unordered_map<int,int> m;
+        
+//         sort(nums.begin(),nums.end());
+        
+//         for (int i = 0; i < n; i++) 
+//             m[nums[i]] = i;
+        
+//         for(int i=0;i<n;i++)
+//         {
+//             if (nums[i] > 0) break;
+            
+//             if(i>0 && nums[i]==nums[i-1])
+//                 continue;
+            
+//             for(int j=i+1;j<n;j++)
+//             {
+//                 if(j!=i+1 && nums[j]==nums[j-1])
+//                     continue;
+                
+//                 if(m[-(nums[i]+nums[j])] > j)
+//                 {
+//                     ans.push_back({nums[i],nums[j],-(nums[i]+nums[j])});
+//                     continue;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
