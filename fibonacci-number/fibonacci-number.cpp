74228db379db490e949fc19e97e4d62fb4c6feb1
Vaishant Makan <vaishantmakan@gmail.com>
2021-11-23 10:52:16 +0530

    Time: 12 ms (26.67%), Space: 6 MB (41.44%) - LeetHub

--- fibonacci-number/fibonacci-number.cpp
+++ fibonacci-number/fibonacci-number.cpp
@@ -1,21 +1,10 @@
-vector<int> dp(31,-1);
-
 class Solution {
 public:
-
     int fib(int n) {
         
-        if(n<1)
-            return 0;
+        if(n <= 1)
+            return n;
         
-        if(n==1 || n==2)
-            return 1;
-        
-        if(dp[n] != -1)
-            return dp[n];
-        
-        dp[n] = fib(n-1)+fib(n-2);
-        
-        return dp[n];
+        return fib(n-1)+fib(n-2);
     }
 };
