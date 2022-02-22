d403912b6efa9cf9f5bb887ff7702b9dc6aad468
Vaishant Makan <vaishantmakan@gmail.com>
2022-02-22 10:50:39 +0530

    Time: 7 ms (35.56%), Space: 12 MB (69.66%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -9,46 +9,78 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// DFS Solution : 
+
 class Solution {
 public:
+    
+    void recursion(TreeNode *root, int level, vector<int> &ans)
+    {
+        if(root==NULL) 
+            return;
+        
+        if(ans.size()==level) 
+            ans.push_back(root->val);
+        
+        recursion(root->right, level+1, ans);
+        recursion(root->left, level+1, ans);
+    }
+    
     vector<int> rightSideView(TreeNode* root) {
         
         vector<int>ans;
         if(root == NULL)
             return ans;
         
-        vector<vector<int>>bfs;
-        
-        queue<TreeNode*>q;
-        q.push(root);
-        
-        while(!q.empty())
-        {
-            int n = q.size();
-            vector<int>level;
-            for(int i=0;i<n;i++)
-            {
-                TreeNode* node = q.front();
-                q.pop();
-                
-                if(node->left != NULL)
-                    q.push(node->left);
-                
-                if(node->right != NULL)
-                    q.push(node->right);
-                
-                level.push_back(node->val);
-            }
-            bfs.push_back(level);
-        }
-        
-        int n=bfs.size();
-        for(int i=0;i<n;i++)
-        {
-            int m=bfs[i].size();
-            ans.push_back(bfs[i][m-1]);
-        }
+        recursion(root, 0, ans);
         
         return ans;
     }
 };
+
+// BFS Solution : 
+
+// class Solution {
+// public:
+//     vector<int> rightSideView(TreeNode* root) {
+        
+//         vector<int>ans;
+//         if(root == NULL)
+//             return ans;
+        
+//         vector<vector<int>>bfs;
+        
+//         queue<TreeNode*>q;
+//         q.push(root);
+        
+//         while(!q.empty())
+//         {
+//             int n = q.size();
+//             vector<int>level;
+//             for(int i=0;i<n;i++)
+//             {
+//                 TreeNode* node = q.front();
+//                 q.pop();
+                
+//                 if(node->left != NULL)
+//                     q.push(node->left);
+                
+//                 if(node->right != NULL)
+//                     q.push(node->right);
+                
+//                 level.push_back(node->val);
+//             }
+//             bfs.push_back(level);
+//         }
+        
+//         int n=bfs.size();
+//         for(int i=0;i<n;i++)
+//         {
+//             int m=bfs[i].size();
+//             ans.push_back(bfs[i][m-1]);
+//         }
+        
+//         return ans;
+//     }
+// };
