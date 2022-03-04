8419ade2db9211b9a67f980a82781b875ec75566
Vaishant Makan <vaishantmakan@gmail.com>
2022-03-04 07:44:05 +0530

    Time: 10 ms (35.76%), Space: 13.7 MB (48.16%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -9,61 +9,85 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// Approach 1 : Using single upper bound range ... https://www.youtube.com/watch?v=UmJT3j26t1I&list=PLgUwDviBIf0q8Hkd7bK2Bpryj2xVJk8Vk&index=49
 class Solution {
 public:
     
-    int helper(TreeNode* root, long minV, long maxV, vector<int>& preorder, int &itr)
-    {
-        if(itr >= preorder.size())
-            return itr;
+    TreeNode* build(vector<int>& preorder, int& i, int bound) {
         
-        int val = preorder[itr];
+        if (i == preorder.size() || preorder[i] > bound) 
+            return NULL;
         
-        // check if val in range 
-        if(val > minV && val < maxV)
-        {
-            int newItr = itr;
-            TreeNode* newNode = new TreeNode(val);
-            if(val < root->val)
-            {
-                root->left = newNode;
-                int temp = itr+1;
-                newItr = helper(root->left, minV, root->val, preorder, temp);
-            }
-            
-            if(newItr >= preorder.size())
-                return itr;
-            
-            itr = newItr;
-            val = preorder[itr];
-            newNode = new TreeNode(val);
-            
-            if(val > minV && val < maxV)
-            {
-                if(val > root->val)
-                {
-                    root->right = newNode;
-                    int temp = itr+1;
-                    itr = helper(root->right, root->val, maxV, preorder, temp);
-                }
-            }
-        }
+        TreeNode* root = new TreeNode(preorder[i++]);
         
-        return itr;
+        root->left = build(preorder, i, root->val);
+        root->right = build(preorder, i, bound);
+        return root;
     }
     
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        
-        if(preorder.size() == 0)
-            return NULL;
-        
-        int val = preorder[0];
-        TreeNode* root = new TreeNode(val);
-        
-        int itr=1;
-        
-        helper(root, LONG_MIN, LONG_MAX, preorder, itr);
-        
-        return root;
+        int i = 0;
+        return build(preorder, i, INT_MAX);
     }
 };
+
+// Approach 2 : Using Both upper bound and lower bound ranges .....
+// class Solution {
+// public:
+    
+//     int helper(TreeNode* root, long minV, long maxV, vector<int>& preorder, int &itr)
+//     {
+//         if(itr >= preorder.size())
+//             return itr;
+        
+//         int val = preorder[itr];
+        
+//         // check if val in range 
+//         if(val > minV && val < maxV)
+//         {
+//             int newItr = itr;
+//             TreeNode* newNode = new TreeNode(val);
+//             if(val < root->val)
+//             {
+//                 root->left = newNode;
+//                 int temp = itr+1;
+//                 newItr = helper(root->left, minV, root->val, preorder, temp);
+//             }
+            
+//             if(newItr >= preorder.size())
+//                 return itr;
+            
+//             itr = newItr;
+//             val = preorder[itr];
+//             newNode = new TreeNode(val);
+            
+//             if(val > minV && val < maxV)
+//             {
+//                 if(val > root->val)
+//                 {
+//                     root->right = newNode;
+//                     int temp = itr+1;
+//                     itr = helper(root->right, root->val, maxV, preorder, temp);
+//                 }
+//             }
+//         }
+        
+//         return itr;
+//     }
+    
+//     TreeNode* bstFromPreorder(vector<int>& preorder) {
+        
+//         if(preorder.size() == 0)
+//             return NULL;
+        
+//         int val = preorder[0];
+//         TreeNode* root = new TreeNode(val);
+        
+//         int itr=1;
+        
+//         helper(root, LONG_MIN, LONG_MAX, preorder, itr);
+        
+//         return root;
+//     }
+// };
