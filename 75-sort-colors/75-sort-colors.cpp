745ef9a5594f9a964538e2f5dd34f03c0d264a21
Vaishant Makan <vaishantmakan@gmail.com>
2022-06-10 12:31:56 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (70.55%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        
+        // one pass and constant space 
+        
+        int n0 = -1, n1 = -1, n2 = -1;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i] == 0)
+            {
+                nums[++n2] = 2;
+                
+                nums[++n1] = 1;
+                
+                nums[++n0] = 0;
+            }
+            
+            else if(nums[i] == 1)
+            {
+                nums[++n2] = 2;
+                
+                nums[++n1] = 1;
+                
+            }
+            
+            else if(nums[i] == 2)
+            {
+                nums[++n2] = 2;
+            }
+        }
+    }
+};
