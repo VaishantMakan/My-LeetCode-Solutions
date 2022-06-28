47603c85dfc693222acb00241c67915f91dfcf8b
Vaishant Makan <vaishantmakan@gmail.com>
2022-06-28 23:15:01 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (23.31%) - LeetHub

---
+++ 394-decode-string/394-decode-string.cpp
@@ -0,0 +1,79 @@
+class Solution {
+public:
+    
+    static string helper(string s, int& index)
+    {
+        string ans = "";
+        
+        if(index >= s.length())
+            return ans;
+        
+        int num = 0;
+        
+        while(s[index] - '0' >= 0 && s[index] - '0' <= 9)
+        {   
+            num = (num*10) + (s[index] - '0');
+            
+            index++;
+        }
+        
+        // if no number 
+        if(num == 0)
+        {
+            if(s[index] == ']')
+            {
+                // index++;
+                // cout<<index<<endl;
+                return ans;
+            }
+            
+            // its a char 
+            ans += s[index];
+            
+            index++;
+            
+            ans += helper(s, index);
+            return ans;
+        }
+        
+        index++; // to skip [
+        
+        while(s[index] != ']')
+        {   
+            if(s[index] - '0' >= 0 && s[index] - '0' <= 9)
+            {
+                ans += helper(s, index);  
+                
+                continue;                
+            }
+            
+            ans += s[index];
+            
+            index++;
+        }
+        
+        index++;
+        
+        string temp = ans;
+        
+        while(num-- != 1)
+        {
+            ans += temp;
+        }
+        
+        if(index < s.length() && s[index] != ']')
+        {   
+            // cout<<"yo: "<<index<<endl;
+            ans += helper(s, index);
+        }
+        
+        return ans;
+    }
+    
+    string decodeString(string s) {
+        
+        int index=0;
+        
+        return helper(s, index);
+    }
+};
