a940b44dd33f7adb804c6b9420bc9b7565c4e3b4
Vaishant Makan <vaishantmakan@gmail.com>
2022-07-03 13:12:57 +0530

    Time: 89 ms (50.36%), Space: 31.9 MB (52.01%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,55 +1,37 @@
 class Solution {
 public:
-    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) 
+    {
+        vector<int> ans;
         
-        int n = arr.size();
+        int n = size(arr);
         
-        vector<int>ans;
-        
-        if(x <= arr[0])
-        {
-            int j=0;
-            while(k--)
-            {
-                ans.push_back(arr[j++]);        
-            }
-            return ans;
-        }
-        
-        if(x >= arr[n-1])
-        {
-            int j=n-k;
-            while(k--)
-            {
-                ans.push_back(arr[j++]);        
-            }
-            return ans;
-        }
-        
-        int start = 0;
-        int end = n-1;
-        
-        int count=0;
-        
-        while(start <= end)
-        {
-            if(end - start + 1 == k)
-            {
-               break;
-            }
+        // gives the index of the element just greater than equal to x 
+        int R = lower_bound(begin(arr), end(arr), x) - begin(arr); 
+
+        int L = R - 1; // this will be the element just smaller than x 
             
-            int d1 = abs(arr[start] - x);
-            int d2 = abs(arr[end] - x);
+        // now we have a possible window of the elemenst closest to x and 
+        // now one by one we will add elements into this window by checking 
+        // or you can say that these L and R are the pointers and we will check them and put acc. 
+        
+	    // expand the (L, R) window till the no. of elements between it are equal to k
+        // This window's starting and ending points are not included yet, its just like the 
+        // range, the elements that come in between it are to be included. 
             
-            if(d1 <= d2)
-                end--;
-            else
-                start++;
+        while(k--) 
+        {
+            if(R >= n || (L >= 0 && x - arr[L] <= arr[R] - x)) 
+                L--;  // expand from left meaning that the current Left element comes inside the window and the left boundary expands 
+            else 
+                R++;  // expand from right meaning that teh current right element comes inside the window and the right boundary expands
         }
         
-        for(int i=start;i<=end;i++)
+        // now add the elements inside the boundary 
+        for(int i=L+1; i<R; i++)
+        {
             ans.push_back(arr[i]);
-        
-        return ans;
+        }
+        return ans;  
     }
 };
