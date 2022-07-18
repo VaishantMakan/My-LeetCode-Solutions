f3094c68760e58f71607eec2ba5d1ebf3fa99508
Vaishant Makan <vaishantmakan@gmail.com>
2022-07-18 13:31:34 +0530

    Time: 60 ms (43.33%), Space: 13.2 MB (73.01%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
@@ -1,9 +1,9 @@
+// DFS + MEMOIZATION 
 class Solution {
 public:
     
-    int helper(int node, int dst, int k, vector<vector<pair<int, int>>>& adj, vector<int>& visited, vector<vector<int>>& dp)
+    int helper(int node, int dst, int k, vector<vector<pair<int, int>>>& adj, vector<vector<int>>& dp)
     {
-        // cout<<"node = "<<node<<endl;
         if(node == dst)
         {
             return 0;
@@ -19,20 +19,12 @@ public:
         
         for(auto& it: adj[node])
         {
-            // if(!visited[it.first])
-            // {
-                // visited[it.first] = 1;
-                
-                int temp = helper(it.first, dst, k-1, adj, visited, dp);
-                
-                // visited[it.first] = 0;
-                
-                if(temp == INT_MAX)
-                    continue;
-                
-                optimalFromHere = min(optimalFromHere, temp + it.second);
-                // cout<<"OF : "<<optimalFromHere<<endl;
-            // }
+            int temp = helper(it.first, dst, k-1, adj, dp);
+
+            if(temp == INT_MAX)
+                continue;
+
+            optimalFromHere = min(optimalFromHere, temp + it.second);
         }
         
         return dp[node][k] = optimalFromHere;
@@ -48,12 +40,9 @@ public:
             adj[it[0]].push_back({it[1], it[2]});
         }
         
-        vector<int> visited(n, 0);
-        visited[src] = 1;
-        
         vector<vector<int>>dp(n, vector<int>(n, -1));
         
-        int ans = helper(src, dst, k, adj, visited, dp);
+        int ans = helper(src, dst, k, adj, dp);
         
         if(ans == INT_MAX)
             return -1;
