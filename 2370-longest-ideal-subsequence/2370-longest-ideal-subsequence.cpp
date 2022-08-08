f8d20c00c349ddd099a4a2fc273978b272e97152
Vaishant Makan <vaishantmakan@gmail.com>
2022-08-08 10:32:59 +0530

    Time: 1016 ms (57.14%), Space: 421.9 MB (42.86%) - LeetHub

--- 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
@@ -1,3 +1,17 @@
+// Why we have taken 150 size for prev in dp array ? => 
+// prev can go from i - k to i+k ... since only valid prevs are to be checked since only they will be passed on in recursion
+// i could be 'z' (122 - Ascii Value) , k at max could be 25
+// so j = i+k = 122 + 25 = 147.  round it off to 150 
+
+//We can even trim down this size to 51 or 52 by sending s[index] - 'a' as prevs as that would scale down ascii values (i) to the range 0 - 25
+// hence j = i+k = 26 + 25 = 51 or 52 to be safe 
+
+// why we are sending s[index] instead of index as prev ? => 
+// because since index can go from 1 - 10e5 , sending index as prev will mean that prev will also range from 1 - 10e5 , hence means dp array of size n*n 
+//hence TC -> O(n*n) ... which is TLE 
+// But it can be observed that we need prev to check the condition abs(s[index] - s[prev]) <= k ... that means we just need the char at prev index instead
+// of prev index . So we can just simply pass the prev char to improve tc , hence the TC -> O(150 * n) .... it can be further optimized to O(26 * n)
+
 class Solution {
 public:
     
@@ -12,9 +26,9 @@ public:
         int take = 0;
         int dontTake = 0;
         
-        if(prev == -1 || abs(s[index] - prev) <= k)
+        if(prev == -1 || abs((s[index] - 'a') - prev) <= k)
         {
-            take = 1 + helper(s, k, index+1, s[index], dp);
+            take = 1 + helper(s, k, index+1, s[index] - 'a', dp);
         }
         
         dontTake = helper(s, k, index+1, prev, dp);
