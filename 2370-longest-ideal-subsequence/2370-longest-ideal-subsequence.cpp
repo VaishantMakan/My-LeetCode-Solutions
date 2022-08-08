0b976e672b81feecf09d03781a86505e191d224f
Vaishant Makan <vaishantmakan@gmail.com>
2022-08-08 09:26:07 +0530

    Time: 1475 ms (28.57%), Space: 475.9 MB (42.86%) - LeetHub

--- 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
@@ -19,3 +19,43 @@ public:
         return solve(s,0,0,k,dp);
     }
 };
+
+// class Solution {
+// public:
+    
+//     int helper(string s, int k, int index, int prev, vector<vector<int>>& dp)
+//     {
+//         if(index >= s.length())
+//             return 0;
+        
+//         if(dp[prev+1][index] != -1)
+//             return dp[prev+1][index];
+        
+//         int take = 0;
+//         int dontTake = 0;
+        
+//         if(prev == -1 || abs(s[index] - prev) <= k)
+//         {
+//             // if(prev != -1)
+//             // {
+//             //     cout<<"hey : "<<s[index] - 'a'<<" "<<s[prev] - 'a'<<endl;
+//             // }
+//             // cout<<s[index]<<" "<<prev<<endl;
+//             take = 1 + helper(s, k, index+1, s[index], dp);
+//         }
+        
+//         dontTake = helper(s, k, index+1, prev, dp);
+        
+//         return dp[prev+1][index] = max(take, dontTake);
+//     }
+    
+//     int longestIdealString(string s, int k) {
+        
+//         int n = s.length();
+        
+//         vector<vector<int>>dp(n+1, vector<int>(n+1,-1));
+        
+//         return helper(s, k, 0, -1, dp);
+        
+//     }
+// };
