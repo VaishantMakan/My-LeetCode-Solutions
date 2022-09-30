d01583bffb7e50db8279b0a063ba989f812e1b21
Vaishant Makan <vaishantmakan@gmail.com>
2022-09-30 07:01:29 +0530

    Time: 73 ms (39.01%), Space: 30.5 MB (6.32%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    int helper(string& s, string& t, int i, int j, vector<vector<int>>& dp)
+    int helperMemo(string& s, string& t, int i, int j, vector<vector<int>>& dp)
     {
         if((i >= s.length() && j >= t.length()) || j >= t.length())
             return 1;
@@ -15,11 +15,45 @@ public:
         //take
         int take=0;
         if(s[i] == t[j])
-            take = helper(s, t, i+1, j+1, dp);
+            take = helperMemo(s, t, i+1, j+1, dp);
         
-        int dont = helper(s, t, i+1, j, dp);
+        int dont = helperMemo(s, t, i+1, j, dp);
         
         return dp[i][j] = take + dont;
+    }
+    
+    int helperTabulation(string s, string t)
+    {
+        int n = s.length();
+        int m = t.length();
+        
+        static int MOD = (int)1e9 + 7;
+        
+        vector<vector<int>>dp(n+1, vector<int>(m+1, 0));
+        
+        for(int i=0;i<=n;i++)
+            dp[i][0] = 1;
+        
+        for(int j=1;j<=m;j++)
+            dp[0][j] = 0;
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                
+                int take=0;
+                
+                if(s[i-1] == t[j-1])
+                    take = dp[i-1][j-1];
+        
+                int dont = dp[i-1][j];
+        
+                dp[i][j] = (take + dont)%MOD;
+            }
+        }
+        
+        return dp[n][m];
     }
     
     int numDistinct(string s, string t) {
@@ -30,6 +64,8 @@ public:
         
         vector<vector<int>>dp(n, vector<int>(m, -1));
         
-        return helper(s, t, i, j, dp);
+        // return helperMemo(s, t, i, j, dp);
+        
+        return helperTabulation(s, t);
     }
 };
