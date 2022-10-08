29c330108d71e9118bb3afa333e6ea6cff061541
Vaishant Makan <vaishantmakan@gmail.com>
2022-10-08 17:05:31 +0530

    Time: 26 ms (42.36%), Space: 13.8 MB (15.07%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -5,40 +5,37 @@
 class Solution {
 public:
     
-    int helper(int transactionsLeft, vector<int>& prices, vector<vector<int>>& dp, int day)
-    {
-        if(day == prices.size() || transactionsLeft == 0)
-            return 0;
-        
-        if(dp[day][transactionsLeft] != -1)
-            return dp[day][transactionsLeft];
-        
-        //no transaction
-        int ans1 = helper(transactionsLeft, prices, dp, day+1);
-        
-        //transaction
-        int ans2=0;
-        
-        bool buy = (transactionsLeft % 2 == 0);
-        
-        if(buy)
-        {
-            ans2 = -prices[day] + helper(transactionsLeft-1, prices, dp, day+1);
-        }
-        else
-        {
-            ans2 = prices[day] + helper(transactionsLeft-1, prices, dp, day+1);
-        }
-        
-        return dp[day][transactionsLeft] = max(ans1, ans2);
-    }
-    
     int maxProfit(int k, vector<int>& prices) {
         
         int n = prices.size();
         
-        vector<vector<int>>dp(n, vector<int>(k*2+1, -1));
+        vector<vector<int>>dp(n+1, vector<int>(k*2+1, 0));
         
-        return helper(k*2, prices, dp, 0);
+        for(int day = n-1; day>=0; day--)
+        {
+            for(int transactionsLeft = k*2-1; transactionsLeft >= 0; transactionsLeft--)
+            {
+                //no transaction
+                int ans1 = dp[day+1][transactionsLeft];
+
+                //transaction
+                int ans2=0;
+
+                bool buy = (transactionsLeft % 2 == 0);
+
+                if(buy)
+                {
+                    ans2 = -prices[day] + dp[day+1][transactionsLeft+1];
+                }
+                else
+                {
+                    ans2 = prices[day] + dp[day+1][transactionsLeft+1];
+                }
+
+                dp[day][transactionsLeft] = max(ans1, ans2);
+            }
+        }
+        
+        return dp[0][0];
     }
 };
