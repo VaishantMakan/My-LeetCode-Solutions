f1ae92ad1e11b33373d763ccafb806123b395f03
Vaishant Makan <vaishantmakan@gmail.com>
2022-11-02 11:48:47 +0530

    Time: 660 ms (36.25%), Space: 10.9 MB (23.78%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,47 +1,56 @@
+// TC -> O(n^2)
+// SC -> O(n)
+// Printing the LIS too
+
 class Solution {
 public:
-    
-//Memoization :
-    
-//     int helper(vector<int>& nums, int index, int prevI, vector<vector<int>>&dp)
-//     {
-//         if(index >= nums.size())
-//             return 0;
-        
-//         if(dp[index][prevI+1] != -1)
-//             return dp[index][prevI+1];
-        
-//         int take = 0;
-//         int dontTake = helper(nums, index+1, prevI, dp);
-        
-//         if(prevI == -1 || nums[index] > nums[prevI])
-//             take = 1 + helper(nums, index+1, index, dp);
-        
-//         return dp[index][prevI+1] = max(take, dontTake);
-//     }
-    
-// Tabulation : 
-    
     int lengthOfLIS(vector<int>& nums) {
-        
+         
         int n = nums.size();
         
-        vector<vector<int>>dp(n+1, vector<int>(n+1, 0));
+        vector<int>dp(n, 1);
         
-        for(int i=n-1;i>=0;i--)
+        vector<int>prev(n);
+        
+        int maxi = 0;
+        int lastIndex = 0;
+        
+        for(int i=0;i<n;i++)
         {
-            for(int j=i-1;j>=-1;j--)
+            prev[i] = i;
+            
+            for(int j=0;j<i;j++)
             {
-                int take = 0;
-                int dontTake = dp[i+1][j+1];
-        
-                if(j == -1 || nums[i] > nums[j])
-                    take = 1 + dp[i+1][i+1];
-        
-                dp[i][j+1] = max(take, dontTake);
+                if((nums[j] < nums[i]) && (1 + dp[j] > dp[i]))
+                {
+                    dp[i] = 1 + dp[j];
+                    prev[i] = j;
+                }
+            }
+
+            if(dp[i] > maxi)
+            {
+                maxi = dp[i];
+                lastIndex = i;
             }
         }
         
-        return dp[0][-1+1];
+        //printing the LIS
+        
+        vector<int>lis;
+        lis.push_back(nums[lastIndex]);
+        
+        while(prev[lastIndex] != lastIndex)
+        {
+            lastIndex = prev[lastIndex];
+            lis.push_back(nums[lastIndex]);
+        }
+        
+        reverse(lis.begin(), lis.end());
+        
+        for(int i=0;i<lis.size();i++)
+            cout<<lis[i]<<" ";
+        
+        return maxi;
     }
 };
