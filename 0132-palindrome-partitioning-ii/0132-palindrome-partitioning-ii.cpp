33482c80c8c15205dd12243f03b499747ea6bf09
Vaishant Makan <vaishantmakan@gmail.com>
2022-11-16 19:20:49 +0530

    Time: 431 ms (49.70%), Space: 6.7 MB (65.89%) - LeetHub

---
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    
+    bool isPalindrome(string& s, int i, int j)
+    {   
+        while(i < j)
+        {
+            if(s[i] != s[j])
+                return false;
+            
+            i++;
+            j--;
+        }
+        
+        return true;
+    }
+    
+    int helper(string& s, int i, int n, vector<int>& dp)
+    {
+        if(i >= n)
+            return 0;
+        
+        if(dp[i] != -1)
+            return dp[i];
+        
+        int mini = 1e9;
+        
+        for(int j=i; j<n; j++)
+        { 
+            if(isPalindrome(s, i, j))
+            {
+                int cost = 1 + helper(s, j+1, n, dp);
+                mini = min(mini, cost);
+            }
+        }
+        
+        return dp[i] = mini;
+    }
+    
+    int minCut(string s) {
+        
+        int n = s.length();
+        
+        vector<int>dp(n, -1);
+        
+        return helper(s, 0, n, dp) - 1;
+    }
+};
